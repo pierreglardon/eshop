42dfaa674d4db4ac8cbf20762ec802616c843812
Pierre Glardon <pierreglardoncontact@gmail.com>
2016-06-01 01:37:34 +0200

    WOW SLIDER ENFIN AMEN

--- wp-content/themes/overwatch/page-templates/front.php
+++ wp-content/themes/overwatch/page-templates/front.php
@@ -8,6 +8,20 @@ hello test
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <section class="intro" role="main">
+	<div class="cycle-slideshow"
+	    data-cycle-fx="scrollHorz"
+	    data-cycle-timeout="0"
+	    data-cycle-prev="#prev"
+	    data-cycle-next="#next"
+	    >
+	    <img src="http://malsup.github.io/images/p1.jpg">
+	    <img src="http://malsup.github.io/images/p2.jpg">
+	    <img src="http://malsup.github.io/images/p3.jpg">
+	    <img src="http://malsup.github.io/images/p4.jpg">
+	</div>
+	<div class="row">
+
+	</div>
 	<div class="fp-intro">
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
@@ -19,7 +33,7 @@ hello test
 				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
-			
+
 		</div>
 
 	</div>
