42b1cfd7ff5e469c40563a1d6e77f75c49a36fce
Pierre Glardon <pierreglardoncontact@gmail.com>
2016-06-01 12:56:20 +0200

    SLIDE

--- wp-content/themes/overwatch/page-templates/front.php
+++ wp-content/themes/overwatch/page-templates/front.php
@@ -4,26 +4,29 @@ Template Name: Front
 */
 get_header(); ?>
 
-hello test
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-<section class="intro" role="main">
+<section class="mainSlider">
 	<div class="cycle-slideshow"
-	    data-cycle-fx="scrollHorz"
-	    data-cycle-timeout="0"
-	    data-cycle-prev="#prev"
-	    data-cycle-next="#next"
+	    data-cycle-fx=scrollHorz
+	    data-cycle-timeout=2000
+	    data-cycle-pager=".example-pager"
 	    >
+	    <!-- empty element for pager links -->
+	    <div class="cycle-pager"></div>
 	    <img src="http://malsup.github.io/images/p1.jpg">
 	    <img src="http://malsup.github.io/images/p2.jpg">
 	    <img src="http://malsup.github.io/images/p3.jpg">
 	    <img src="http://malsup.github.io/images/p4.jpg">
 	</div>
-	<div class="row">
+	<div class="example-pager"></div>
+</section>
 
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+<section class="intro" role="main">
+	<div class="row">
 	</div>
 	<div class="fp-intro">
-
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 			<div class="entry-content">
@@ -33,13 +36,10 @@ hello test
 				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
-
 		</div>
-
 	</div>
-
 </section>
+
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
-
 <?php get_footer();
