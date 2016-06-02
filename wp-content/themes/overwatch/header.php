e732f2f6c047553898e44d74a2431dd2faebf3d9
Pierre Glardon <pierreglardoncontact@gmail.com>
2016-06-03 00:24:37 +0200

    jeveuxdormir

--- wp-content/themes/overwatch/header.php
+++ wp-content/themes/overwatch/header.php
@@ -19,12 +19,6 @@
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
-	<div class="off-canvas-wrapper">
-		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
-
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header id="header">
--- wp-content/themes/overwatch/page-templates/front.php
+++ wp-content/themes/overwatch/page-templates/front.php
@@ -6,40 +6,50 @@ get_header(); ?>
 
 <section class="mainSlider">
 	<div class="cycle-slideshow"
+		data-cycle-swipe=true
+	    data-cycle-swipe-fx=scrollHorz
 	    data-cycle-fx=scrollHorz
-	    data-cycle-timeout=2000
-	    data-cycle-pager=".example-pager"
+	    data-cycle-timeout=3000
+	    data-cycle-pager=".pager"
+		data-cycle-pager-template="<span></span>"
 	    >
 	    <!-- empty element for pager links -->
 	    <div class="cycle-pager"></div>
-	    <img src="http://malsup.github.io/images/p1.jpg">
-	    <img src="http://malsup.github.io/images/p2.jpg">
-	    <img src="http://malsup.github.io/images/p3.jpg">
-	    <img src="http://malsup.github.io/images/p4.jpg">
+	    <img src="<?php bloginfo('template_directory') ?>/assets/images/slider.jpg" />
+	    <img src="<?php bloginfo('template_directory') ?>/assets/images/slider.jpg" />
+	    <img src="<?php bloginfo('template_directory') ?>/assets/images/slider.jpg" />
 	</div>
-	<div class="example-pager"></div>
+	<div class="pager"></div>
 </section>
-
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-
-<section class="intro" role="main">
+<section class="punchline">
+	<h2>
+		<div>“S’offrir une montre ne doit</div>
+		<div>plus vous coûter le bras qui la porte.”</div>
+		<aside>— Robert, fondateur de <span>focus</span>.com </aside>
+	</h2>
+</section>
+<section class="newproducts">
 	<div class="row">
+		<div class="columns small-12">
+			<h3>Découvrez nos nouveaux produits</h3>
+		</div>
 	</div>
-	<div class="fp-intro">
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
+	<div class="row">
+		<div class="columns small-12">
+			<?php the_field('produits_recents'); ?>
 		</div>
 	</div>
 </section>
 
-<?php endwhile;?>
+	<?php do_action( 'foundationpress_before_content' ); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+<aside class="">
+
+		test
+
+</aside>
+	<?php endwhile;?>
+
+
 <?php do_action( 'foundationpress_after_content' ); ?>
 <?php get_footer();
